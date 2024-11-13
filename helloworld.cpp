a574ff695aac2584041e0793fdd8b22272d0e4fe
ErnestLoo <160474347+ErnestLoo@users.noreply.github.com>
2024-11-14 02:04:11 +0800

    git testing

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+  //This is a comment
+  /*
+  multi-line comment
+  */
+
+  // std::cout << "I like pizza!" << '\n';
+  // std::cout << "It's really good" << '\n';
+  
+  int x = 5;
+  int y = 6;
+  int sum = x + y;
+
+  std::cout << x << '\n';
+  std::cout << y << '\n';
+  std::cout << sum << '\n';
+  
+  return 0;
+}
